1a2f4e12249f620b7e560ca2c8015a06292e2586
coleyrockin <coleyrockin@aol.com>
2022-10-17 17:59:52 -0500

    12.6 done

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,6 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
 const { signToken } = require('../utils/auth');
 
-
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
@@ -37,11 +36,28 @@ const resolvers = {
       return Thought.findOne({ _id });
     }
   },
+
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
       const token = signToken(user);
 
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
       return { token, user };
     },
     addThought: async (parent, args, context) => {
@@ -84,22 +100,6 @@ const resolvers = {
       }
 
       throw new AuthenticationError('You need to be logged in!');
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-      return { token, user };
     }
   }
 };
