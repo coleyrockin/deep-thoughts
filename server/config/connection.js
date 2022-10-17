01519a606126b9b7668839780dff525489404edf
coleyrockin <coleyrockin@aol.com>
2022-10-17 18:20:36 -0500

    server change, deployment issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
