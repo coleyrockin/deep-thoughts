eff06c53fdf29573837b935613891165a4b3a11f
coleyrockin <coleyrockin@aol.com>
2022-10-17 18:10:47 -0500

    script

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,4 +7,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
   useFindAndModify: false
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 module.exports = mongoose.connection;
